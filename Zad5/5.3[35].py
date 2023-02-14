9ebfa1bac7c1a19d70c4f6dbfd42f3c99807a833
Margarita <margoshka_d@mail.ru>
2023-02-14 23:04:21 +0300

    Напишите функцию, которая принимает число и проверяет, является ли оно простым

---
+++ Zad5/5.3[35].py
@@ -0,0 +1,13 @@
+# №5.3[35]. Напишите функцию, которая принимает число и проверяет, является ли оно простым
+# Напоминание: Простое число - это число, которое имеет 2 делителя: 1 и само число
+# Если число простое - функция возвращает True, если нет - возвращает False
+# Примеры/Тесты:
+# <function_name>(13) -> True
+# <function_name>(10) -> False
+
+def is_prime_number(n:int) -> bool:   #is_prime_number-является ли целым числом def is_prime_number(n):
+    for i in range(2, n):
+        if n % i == 0: #делим число без остатка
+            return False            
+    return True
+print(is_prime_number(10))
